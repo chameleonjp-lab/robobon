cbac1365e892b45bc48b2b46522ce0a2dbd9610d
chameleonJP <deccho1026@icloud.com>
2026-08-14 10:25:22 +0900

    P1-06: export motion and sensor foundations

--- src/simulation/index.ts
+++ src/simulation/index.ts
@@ -1,4 +1,7 @@
 export * from './clock';
 export * from './fixed-point';
+export * from './geometry';
+export * from './motion';
 export * from './prng';
+export * from './sensor';
 export * from './state-hash';
